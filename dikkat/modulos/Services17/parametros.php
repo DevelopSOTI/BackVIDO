8eb833a623c7f9812522a26b2d941a31e4327cb8
osvaldoe27 <desarrollo@soti.com.mx>
2024-03-21 17:05:07 -0600

    se agrego web service parametros

--- dikkat/modulos/Services17/parametros.php
+++ dikkat/modulos/Services17/parametros.php
@@ -6,6 +6,7 @@ function getParametros($NOMBRE,$MODULO,$BD){
     if ($conn){
         // <editor-fold defaultstate="collapsed" desc="SELECCION DE LOS DATOS DE LAS CATEGORIAS DEL DEPARTAMETNO EN EL SISTEMA">
         $select  = "SELECT * FROM PARAMETROS WHERE NOMBRE='$NOMBRE' AND MODULO='$MODULO' ;";
+        echo $select;
         // </editor-fold>  
         //echo " Consulta ".$select." ";
         $stmt = mysqli_query($conn, $select);
