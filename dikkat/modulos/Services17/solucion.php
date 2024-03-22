86708a07d8c8ee1781165ae5956b02ca334fcab8
osvaldoe27 <desarrollo@soti.com.mx>
2024-03-22 12:25:36 -0600

    se agrego web service mostar faltantes

--- dikkat/modulos/Services17/solucion.php
+++ dikkat/modulos/Services17/solucion.php
@@ -86,7 +86,7 @@ function getSolucionOpciones($BD)
         $stmt = mysqli_query($conn, $select);
         if ($stmt) {
 
-            $i =  mysqli_num_rows($stmt);
+            $i = mysqli_num_rows($stmt);
             if ($i > 0)
                 unset($SolucionOpciones);
 
@@ -102,7 +102,7 @@ function getSolucionOpciones($BD)
             mysqli_close($conn);
             return $SolucionOpciones;
         }
-        
+
     } else {
         // FALLO LA CONEXION
         return $SolucionOpciones;
@@ -134,10 +134,99 @@ $server->wsdl->addComplexType(
 $server->register(
     'getSolucionOpciones',
     array(
-        
+
         'BD' => 'xsd:string'
     ),
     array('return' => 'tns:SolucionOpcionesArray'),
+    $namespace,
+    false,
+    'rpc',
+    false,
+    'Devuelve un arreglo con los articulos faltanes en el sistema en una fecha y sucursal determinada'
+);
+
+
+function MostrarFaltantesPendiente($FALTANTES_ID, $SUCURSAL_ID, $BD)
+{
+    $conn = ABRIR_CONEXION_MYSQL(FALSE, $BD);
+    $result = null;
+    $hostname = $_SERVER['SERVER_NAME'];
+    if ($conn) {
+        // <editor-fold defaultstate="collapsed" desc="SELECCION DE LOS DATOS DE LAS CATEGORIAS DEL DEPARTAMETNO EN EL SISTEMA">
+        $select = "SELECT F.FALTANTES_ID,FD.FALTANTES_DETALLE_ID,A.ARTICULO_ID,A.SKU,A.NOMBRE,A.DESCRIPCION,FD.STOCK_FISICO,FD.PRECIO_ARTICULO,A.IMAGEN,D.NOMBRE AS CATEGORIA ";
+        $select .= " FROM FALTANTES AS F ";
+        $select .= " INNER JOIN FALTANTES_DETALLE AS FD ON F.FALTANTES_ID =FD.FALTANTES_ID ";
+        $select .= " INNER JOIN ARTICULOS AS A ON FD.ARTICULO_ID=A.ARTICULO_ID ";
+        $select .= " INNER JOIN CATEGORIAS AS C ON A.CATEGORIA_ID=C.CATEGORIA_ID";
+        $select .= " INNER JOIN DEPARTAMENTOS AS D ON C.DEPARTAMENTO_ID=D.DEPARTAMENTO_ID ";
+        $select .= " WHERE F.FALTANTES_ID='$FALTANTES_ID' AND F.SUCURSAL_ID=$SUCURSAL_ID AND F.ESTATUS ='P' ORDER BY FD.FALTANTES_DETALLE_ID DESC;";
+        // </editor-fold>    
+        $stmt = mysqli_query($conn, $select);
+        if ($stmt) {
+            while ($row = mysqli_fetch_assoc($stmt)) {
+                $faltante["FALTANTES_ID"] = $row["FALTANTES_ID"];
+                $faltante["FALTANTES_DETALLE_ID"] = $row["FALTANTES_DETALLE_ID"];
+                $faltante["ARTICULO_ID"] = $row["ARTICULO_ID"];
+                $faltante["SKU"] = $row["SKU"];
+                $faltante["NOMBRE"] = $row["NOMBRE"];
+                $faltante["DESCRIPCION"] = $row["DESCRIPCION"];
+                $faltante["STOCK_FISICO"] = $row["STOCK_FISICO"];
+                $faltante["PRECIO_ARTICULO"] = $row["PRECIO_ARTICULO"];
+                $faltante["IMAGEN"] = $hostname . "/articulos/" . $row["IMAGEN"];
+                $faltante["CATEGORIA"] = $row["CATEGORIA"];
+                $result[] = $faltante;
+            }
+            mysqli_close($conn);
+            return $result;
+        } else {
+            mysqli_close($conn);
+            return $result;
+        }
+        mysqli_close($conn);
+    } else {
+        // FALLO LA CONEXION
+        return $result;
+    }
+}
+
+
+$server->wsdl->addComplexType(
+    'MostrarSolucion',
+    'complexType',
+    'struct',
+    'all',
+    '',
+    array(
+        'FALTANTES_ID' => array('name' => 'FALTANTES_ID', 'type' => 'xsd:int'),
+        'FALTANTES_DETALLE_ID' => array('name' => 'FALTANTES_DETALLE_ID', 'type' => 'xsd:int'),
+        'ARTICULO_ID' => array('name' => 'ARTICULO_ID', 'type' => 'xsd:int'),
+        'SKU' => array('name' => 'SKU', 'type' => 'xsd:string'),
+        'NOMBRE' => array('name' => 'NOMBRE', 'type' => 'xsd:string'),
+        'DESCRIPCION' => array('name' => 'DESCRIPCION', 'type' => 'xsd:string'),
+        'STOCK_FISICO' => array('name' => 'STOCK_FISICO', 'type' => 'xsd:string'),
+        'PRECIO_ARTICULO' => array('name' => 'PRECIO_ARTICULO', 'type' => 'xsd:string'),
+        'IMAGEN' => array('name' => 'IMAGEN', 'type' => 'xsd:string'),
+        'CATEGORIA' => array('name' => 'CATEGORIA', 'type' => 'xsd:string')
+    )
+);
+$server->wsdl->addComplexType(
+    'MostrarSolucionArray',
+    'complexType',
+    'array',
+    '',
+    'SOAP-ENC:Array',
+    array(),
+    array(array('ref' => 'SOAP-ENC:arrayType', 'wsdl:arrayType' => 'tns:MostrarSolucion[]')),
+    'tns:MostrarSolucion'
+);
+$server->register(
+    'MostrarFaltantesPendiente',
+    array(
+        'FALTANTES_ID' => 'xsd:string',
+        'SUCURSAL_ID' => 'xsd:int',
+        'BD' => 'xsd:string'
+    ),
+    array('return' => 'tns:MostrarSolucionArray'),
     $namespace,
     false,
     'rpc',
