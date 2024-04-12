f4ba68272189b6638ada102a6c6e6d38b08f2a5e
osvaldoe27 <desarrollo@soti.com.mx>
2024-04-12 16:46:41 -0600

    cambio de consulta en solucion

--- dikkat/modulos/Services17/solucion.php
+++ dikkat/modulos/Services17/solucion.php
@@ -153,13 +153,42 @@ function MostrarFaltantesPendiente($FALTANTES_ID, $SUCURSAL_ID, $BD)
     $hostname = $_SERVER['SERVER_NAME'];
     if ($conn) {
         // <editor-fold defaultstate="collapsed" desc="SELECCION DE LOS DATOS DE LAS CATEGORIAS DEL DEPARTAMETNO EN EL SISTEMA">
-        $select = "SELECT F.FALTANTES_ID,FD.FALTANTES_DETALLE_ID,A.ARTICULO_ID,A.SKU,A.NOMBRE,A.DESCRIPCION,FD.STOCK_FISICO,FD.PRECIO_ARTICULO,A.IMAGEN,D.NOMBRE AS CATEGORIA ";
-        $select .= " FROM FALTANTES AS F ";
-        $select .= " INNER JOIN FALTANTES_DETALLE AS FD ON F.FALTANTES_ID =FD.FALTANTES_ID ";
-        $select .= " INNER JOIN ARTICULOS AS A ON FD.ARTICULO_ID=A.ARTICULO_ID ";
-        $select .= " INNER JOIN CATEGORIAS AS C ON A.CATEGORIA_ID=C.CATEGORIA_ID";
-        $select .= " INNER JOIN DEPARTAMENTOS AS D ON C.DEPARTAMENTO_ID=D.DEPARTAMENTO_ID ";
-        $select .= " WHERE F.FALTANTES_ID='$FALTANTES_ID' AND F.SUCURSAL_ID=$SUCURSAL_ID AND F.ESTATUS ='P' ORDER BY FD.FALTANTES_DETALLE_ID DESC;";
+        $select = "SELECT S.*";
+        $select .= " FROM(";
+        $select .= "     SELECT";
+        $select .= "         F.FALTANTES_ID,";
+        $select .= "         FD.FALTANTES_DETALLE_ID,";
+        $select .= "         A.ARTICULO_ID,";
+        $select .= "         A.SKU,";
+        $select .= "         A.NOMBRE,";
+        $select .= "         A.DESCRIPCION,";
+        $select .= "         FD.STOCK_FISICO,";
+        $select .= "         FD.PRECIO_ARTICULO,";
+        $select .= "         A.IMAGEN,";
+        $select .= "         ,SP.NOMBRE SOLUCION ";
+        $select .= "         F.FECHA,";
+        $select .= "         (";
+        $select .= "             SELECT E.EXISTENCIA";
+        $select .= "             FROM EXISTENCIAS E";
+        $select .= "             WHERE E.ARTICULO_ID = FD.ARTICULO_ID";
+        $select .= "             AND E.SUCURSAL_ID = F.SUCURSAL_ID";
+        $select .= "             AND E.FECHA >= F.FECHA";
+        $select .= "             ORDER BY E.FECHA";
+        $select .= "             LIMIT 1";
+        $select .= "         ) AS EXISTENCIA_TEORICA";
+        $select .= "     FROM FALTANTES AS F";
+        $select .= "     INNER JOIN FALTANTES_DETALLE AS FD ON F.FALTANTES_ID = FD.FALTANTES_ID";
+        $select .= "     INNER JOIN ARTICULOS AS A ON FD.ARTICULO_ID = A.ARTICULO_ID";
+        $select .= "     INNER JOIN SOLUCION SOL on SOL.FALTANTES_ID = F.FALTANTES_ID ";
+        $select .= "     INNER JOIN SOLUCION_DETALLE SD on SD.SOLUCION_ID = SOL.SOLUCION_ID AND SD.ARTICULO_ID = FD.ARTICULO_ID";
+        $select .= "     LEFT JOIN SOLUCION_OPCIONES SP on SP.SOLUCION_OPCIONES_ID = SD.SOLUCION_OPCIONES_ID";
+        $select .= "     WHERE F.FALTANTES_ID = $FALTANTES_ID ";
+        $select .= "     AND F.SUCURSAL_ID = $SUCURSAL_ID ";
+        $select .= "     AND F.ESTATUS = 'P'";
+        $select .= " ) AS S";
+        $select .= " WHERE (S.EXISTENCIA_TEORICA - S.STOCK_FISICO) > 0";
+        $select .= " ORDER BY S.FALTANTES_DETALLE_ID DESC;";
+
         // </editor-fold>    
         $stmt = mysqli_query($conn, $select);
         if ($stmt) {
@@ -172,9 +201,10 @@ function MostrarFaltantesPendiente($FALTANTES_ID, $SUCURSAL_ID, $BD)
                 $faltante["DESCRIPCION"] = $row["DESCRIPCION"];
                 $faltante["STOCK_FISICO"] = $row["STOCK_FISICO"];
                 $faltante["PRECIO_ARTICULO"] = $row["PRECIO_ARTICULO"];
-                $faltante["IMAGEN"] = $hostname . "/articulos/" . $row["IMAGEN"];
-                $faltante["CATEGORIA"] = $row["CATEGORIA"];
+                $faltante["IMAGEN"] = $hostname . "/articulos/" . $row["IMAGEN"];                
                 $faltante["SOLUCION"] = "";
+                $faltante["FECHA"] = $row["FECHA"];
+                $faltante["EXISTENCIA_TEORICA"] = $row["EXISTENCIA_TEORICA"];
                 $result[] = $faltante;
             }
             mysqli_close($conn);
@@ -208,7 +238,10 @@ $server->wsdl->addComplexType(
         'PRECIO_ARTICULO' => array('name' => 'PRECIO_ARTICULO', 'type' => 'xsd:string'),
         'IMAGEN' => array('name' => 'IMAGEN', 'type' => 'xsd:string'),
         'CATEGORIA' => array('name' => 'CATEGORIA', 'type' => 'xsd:string'),
-        'SOLUCION' => array('name' => 'SOLUCION', 'type' => 'xsd:string')
+        'SOLUCION' => array('name' => 'SOLUCION', 'type' => 'xsd:string'),
+        'FECHA' => array('name' => 'FECHA', 'type' => 'xsd:string'),
+        'EXISTENCIA_TEORICA' => array('name' => 'EXISTENCIA_TEORICA', 'type' => 'xsd:string')
+
     )
 );
 $server->wsdl->addComplexType(
@@ -261,7 +294,7 @@ function InsertarSolucion($FECHA, $SUCURSAL_ID, $FALTANTES_ID, $USUARIO_CREACION
             //insertamos el encabezado 
             $query = "INSERT INTO SOLUCION (SUCURSAL_ID,ESTATUS,FECHA,USUARIO_CREACION,FECHA_HORA_CREACION,FALTANTES_ID) ";
             $query .= "VALUES ($SUCURSAL_ID,'A','$FECHA','$USUARIO_CREACION','$FECHA_HORA_CREACION',$FALTANTES_ID);";
-           // echo $query;
+            // echo $query;
             if (mysqli_query($conn, $query)) {
                 $result = 1;
             } else {
@@ -322,7 +355,7 @@ $server->register(
     array(
         'FECHA' => 'xsd:string',
         'SUCURSAL_ID' => 'xsd:int',
-        'FALTANTES_ID'=>'xsd:int',
+        'FALTANTES_ID' => 'xsd:int',
         'USUARIO_CREACION' => 'xsd:string',
         'FECHA_HORA_CREACION' => 'xsd:string',
         'ARTICULO_ID' => 'xsd:string',
@@ -369,7 +402,7 @@ function BuscarSolucionID($FECHA, $SUCURSAL_ID, $FALTANTES_ID, $conn)
 
 function BuscarSolucionIDs($FECHA, $SUCURSAL_ID, $FALTANTES_ID, $BD)
 {
-    $conn = ABRIR_CONEXION_MYSQL(FALSE,$BD);
+    $conn = ABRIR_CONEXION_MYSQL(FALSE, $BD);
     $result = 0;
     if ($conn) {
         // <editor-fold defaultstate="collapsed" desc="SELECCION DE LOS DATOS DE LAS CATEGORIAS DEL DEPARTAMETNO EN EL SISTEMA">
@@ -400,7 +433,7 @@ $server->register(
     array(
         'FECHA' => 'xsd:string',
         'SUCURSAL_ID' => 'xsd:int',
-        'FALTANTES_ID'=>'xsd:int',
+        'FALTANTES_ID' => 'xsd:int',
         'BD' => 'xsd:string'
 
     ),
@@ -442,7 +475,7 @@ function BuscarSolucionDetalleID($SOLUCION_ID, $ARTICULO_ID, $BD)
 
 
 
-function ActualizarHoraInicioFinSolucion($TIPO, $HORA,$FALTANTES_ID, $SOLUCION_ID, $USUARIO_MODIFICACION, $FECHA_HORA_MODIFICACION, $BD)
+function ActualizarHoraInicioFinSolucion($TIPO, $HORA, $FALTANTES_ID, $SOLUCION_ID, $USUARIO_MODIFICACION, $FECHA_HORA_MODIFICACION, $BD)
 {
     $conn = ABRIR_CONEXION_MYSQL(FALSE, $BD);
     $result = false;
@@ -466,7 +499,7 @@ function ActualizarHoraInicioFinSolucion($TIPO, $HORA,$FALTANTES_ID, $SOLUCION_I
             $query .= " USUARIO_MODIFICACION = '$USUARIO_MODIFICACION', ";
             $query .= " FECHA_HORA_MODIFICACION ='$FECHA_HORA_MODIFICACION' ";
             $query .= " WHERE SOLUCION_ID = $SOLUCION_ID AND FALTANTES_ID = $FALTANTES_ID $WHERE_COMPLEMENTO;";
-           // echo $query;
+            // echo $query;
             if (mysqli_query($conn, $query)) {
                 $result = true;
                 mysqli_commit($conn);
