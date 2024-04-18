b44808699cc3309dc51e0ad23b419ea334a61d2b
osvaldoe27 <desarrollo@soti.com.mx>
2024-04-18 12:25:02 -0600

    reporte faltantes

--- dikkat/modulos/Services17/solucion.php
+++ dikkat/modulos/Services17/solucion.php
@@ -383,6 +383,7 @@ function BuscarSolucionID($FECHA, $SUCURSAL_ID, $FALTANTES_ID, $conn)
         if($FECHA === "0"){
             $select .= " AND FECHA='$FECHA' ";
         } 
+        $select .= "ORDER BY SOLUCION_ID DESC LIMIT 1";
         // </editor-fold>  
         //echo " Consulta ".$select." ";
         $stmt = mysqli_query($conn, $select);
